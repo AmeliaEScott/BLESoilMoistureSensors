a8b67072b423370c588c9fe0ecfa5e2719d3c5e4
Amelia Scott <ameliascott1024@gmail.com>
2023-06-30 22:53:02 -0400

    Friendship ended with Softdevice. Now bare metal is my best friend

--- soil_sensor_firmware/memory.x
+++ soil_sensor_firmware/memory.x
@@ -2,8 +2,8 @@ MEMORY
 {
   /* NOTE 1 K = 1 KiBi = 1024 bytes */
   /* These values correspond to the NRF52840 with Softdevices S140 7.0.1 */
-  /* FLASH : ORIGIN = 0x00000000 + 100k, LENGTH = 192K - 100k */
-  /* RAM : ORIGIN = 0x20000000 + 0x2f38, LENGTH = 24K - 0x2f38 */
-  FLASH : ORIGIN = 0x00000000 + 100k, LENGTH = 192k - 92k
-  RAM : ORIGIN = 0x20000000 + 0x2f38, LENGTH = 24k - 0x2f38
+  /* FLASH : ORIGIN = 0x00000000 + 100k, LENGTH = 192k - 92k */
+  /* RAM : ORIGIN = 0x20000000 + 0x2f38, LENGTH = 24k - 0x2f38 */
+  FLASH : ORIGIN = 0x00000000, LENGTH = 192k
+  RAM : ORIGIN = 0x20000000, LENGTH = 24k
 }
--- soil_sensor_firmware/src/main.rs
+++ soil_sensor_firmware/src/main.rs
@@ -17,7 +17,10 @@ use hal::pac;
 mod app {
     use cortex_m::peripheral::syst::SystClkSource;
     use nrf52810_hal::pac::clock::lfclksrc::{BYPASS_A, EXTERNAL_A, SRC_A};
+    use nrf52810_hal::pac::nvmc::config::WEN_A;
+    use nrf52810_hal::pac::p0::pin_cnf::PULL_A;
     use nrf52810_hal::pac::power::systemoff::SYSTEMOFF_AW;
+    use nrf52810_hal::pac::uicr::pselreset::CONNECT_A;
     use super::*;
 
     #[shared]
@@ -32,11 +35,29 @@ mod app {
     fn init(_: init::Context) -> (Shared, Local) {
         let p: pac::Peripherals = pac::Peripherals::take().unwrap();
 
+        p.P0.dir.write(|w| w.pin21().clear_bit());
+        p.P0.pin_cnf[21].write(|w| {
+            w.dir().clear_bit()
+                .input().clear_bit()
+                .pull().variant(PULL_A::PULLUP)
+        });
+
+        p.NVMC.config.write(|w| w.wen().variant(WEN_A::WEN));
+        p.NVMC.eraseuicr.write(|w| w.eraseuicr().set_bit());
+        while !p.NVMC.ready.read().ready().bit() {}
+        // p.UICR.pselreset[0].write(|w| w.pin().variant(21).connect().variant(CONNECT_A::CONNECTED));
+        p.UICR.pselreset[0].write(|w| unsafe{w.bits(0b01111111_11111111_11111111_11010101)});
+        while !p.NVMC.ready.read().ready().bit() {}
+        // p.UICR.pselreset[1].write(|w| w.pin().variant(21).connect().variant(CONNECT_A::CONNECTED));
+        p.UICR.pselreset[1].write(|w| unsafe{w.bits(0b01111111_11111111_11111111_11010101)});
+        while !p.NVMC.ready.read().ready().bit() {}
+        p.NVMC.config.write(|w| w.wen().variant(WEN_A::REN));
+        while !p.NVMC.ready.read().ready().bit() {}
+        
         p.POWER.dcdcen.write(|w| w.dcdcen().set_bit());
         p.POWER.tasks_lowpwr.write(|w| w.tasks_lowpwr().set_bit());
         p.CLOCK.lfclksrc.write(|w| w.src().variant(SRC_A::XTAL).external().variant(EXTERNAL_A::DISABLED).bypass().variant(BYPASS_A::DISABLED));
         p.CLOCK.tasks_hfclkstart.write(|w| w.tasks_hfclkstart().set_bit());
-
 
         (
             Shared {},
@@ -45,8 +66,13 @@ mod app {
     }
 
     #[idle(local = [p])]
-    fn idle(_: idle::Context) -> ! {
+    fn idle(cx: idle::Context) -> ! {
+        let p: &mut pac::Peripherals = cx.local.p;
+
+
+
         loop {
+            defmt::info!("Idle");
             // When using WFI, high power usage (~0.1mA - 1mA)
             rtic::export::wfi();
             // When using System OFF mode, low power usage (~0.001mA)
