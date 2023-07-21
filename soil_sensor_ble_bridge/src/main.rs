32dad8b9428111b848ec2b23079a65dff0d87a14
Amelia Scott <ameliascott1024@gmail.com>
2023-07-20 22:17:57 -0400

    A better BLE bridge

--- soil_sensor_ble_bridge/src/main.rs
+++ soil_sensor_ble_bridge/src/main.rs
@@ -1,67 +1,51 @@
+#![feature(try_blocks)]
 
 use bluer;
 use std::error::Error;
-use tokio::time;
 use uuid::Uuid;
-use log::{debug, info};
-use soil_sensor_common::Measurement;
-
+use log::{debug, info, warn, error};
+use serde::{Serialize, Deserialize};
 use bluer::{Adapter, AdapterEvent, Address, Device, DeviceEvent, DeviceProperty, DiscoveryFilter, DiscoveryTransport};
 use futures::{pin_mut, stream::SelectAll, StreamExt};
 use std::{collections::HashSet, env};
-
-
-/// Only devices whose name contains this string will be tried.
-const PERIPHERAL_NAME_MATCH_FILTER: &str = "HelloRust";
-/// UUID of the characteristic for which we should subscribe to notifications.
-const NOTIFY_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x866a5627_a761_47cc_9976_7457450e8258);
+use soil_sensor_common::Measurement;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> bluer::Result<()> {
     pretty_env_logger::init();
 
-    let with_changes = true;
-    let all_properties = true;
-    let br_edr_only = env::args().any(|arg| arg == "--bredr");
-    let filter_addr: HashSet<_> = env::args().filter_map(|arg| arg.parse::<Address>().ok()).collect();
+    let result = listen_adapter().await;
+    warn!("listen_adapter returned {:?}", result);
+    result
+}
 
+async fn listen_adapter() -> bluer::Result<()> {
     let session = bluer::Session::new().await?;
     let adapter = session.default_adapter().await?;
-    println!("Discovering devices using Bluetooth adapter {}\n", adapter.name());
+    debug!("Discovering devices using Bluetooth adapter {}\n", adapter.name());
     adapter.set_powered(true).await?;
 
     let filter = DiscoveryFilter {
         transport: DiscoveryTransport::Le,
-        pattern: None,
         ..Default::default()
     };
     adapter.set_discovery_filter(filter).await?;
-    println!("Using discovery filter:\n{:#?}\n\n", adapter.discovery_filter().await);
+    debug!("Using discovery filter:\n{:#?}\n\n", adapter.discovery_filter().await);
 
     let device_events = adapter.discover_devices().await?;
     pin_mut!(device_events);
 
     loop {
-        if let Some(device_event) = device_events.next().await {
-            match device_event {
-                AdapterEvent::DeviceAdded(addr) => {
-                    if !filter_addr.is_empty() && !filter_addr.contains(&addr) {
-                        continue;
-                    }
-
-                    debug!("Device added: {addr}");
-                    let device = adapter.device(addr)?;
-                    tokio::spawn(watch_device(device));
-                }
-                AdapterEvent::DeviceRemoved(addr) => {
-                    println!("Device removed: {addr}");
-                }
-                _ => (),
-            }
+        debug!("Waiting for device event...");
+        let event = device_events.next().await;
+        if let Some(AdapterEvent::DeviceAdded(addr)) = event {
+            debug!("Device added: {addr}");
+            let device = adapter.device(addr)?;
+            tokio::spawn(watch_device(device));
+        } else {
+            debug!("Device Event: {:?}", event);
         }
     }
-
-    Ok(())
 }
 
 pub async fn watch_device(device: Device) -> bluer::Result<()> {
@@ -81,25 +65,47 @@ pub async fn watch_device(device: Device) -> bluer::Result<()> {
         return Ok(())
     }
 
-    let mut last_meas: Option<Measurement> = None;
+    let mut last_meas: Option<soil_sensor_common::Measurement> = None;
 
     while let Some(event) = events.next().await {
         if let DeviceEvent::PropertyChanged(DeviceProperty::ManufacturerData(data)) = event {
-            debug!("New data for {}: {:?}", name, data);
-            let bytes = data.get(&65535).unwrap();
-            let measurement = Measurement::from_bytes(soil_sensor_common::Serialized::try_from(bytes.as_slice()).unwrap());
-            if Some(measurement) != last_meas {
-                tokio::spawn(handle_measurement(measurement));
-            } else {
-                debug!("Duplicate measurement: {:?}", measurement);
-            }
-            last_meas = Some(measurement);
+            let result: Result<(), String> = try {
+                let id = u16::from_be_bytes(soil_sensor_common::COMPANY_ID_CODE);
+                let bytes = data.get(&id).ok_or(format!("Data {:?} has no key {}", data, id))?;
+                let bytes = soil_sensor_common::Serialized::try_from(
+                    bytes.as_slice())
+                    .or(Err(format!("Error converting {:?} to Serialized", bytes)))?;
+                let measurement = Measurement::from_bytes(bytes, device.address().0);
+
+                if Some(measurement) != last_meas {
+                    tokio::spawn(handle_measurement(measurement, device.address()));
+                } else {
+                    debug!("Duplicate measurement: {:?}", measurement);
+                }
+                last_meas = Some(measurement);
+                ()
+            };
+
+            debug!("Received new Manufacturer data from {}: {:?}, Result: {:?}",
+                device.address(), data, result);
         }
     }
 
     Ok(())
 }
 
-pub async fn handle_measurement(meas: Measurement) {
-    info!("Cool new measurement: {:?}", meas);
+pub async fn handle_measurement(meas: soil_sensor_common::Measurement, addr: Address) {
+    let now = time::OffsetDateTime::now_local().unwrap_or_else(|_|{
+        time::OffsetDateTime::now_utc()
+    });
+
+    let meas = Measurement {
+        measurement: meas,
+        time: now,
+        sensor_address: addr.0
+    };
+
+    let json = serde_json::to_string_pretty(&meas).unwrap_or("error".to_string());
+
+    info!("Cool new measurement: {}", json);
 }
